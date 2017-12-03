807164989186ae336fd8486a884ad1b18ce00840
Shonamd <shonamdoran@gmail.com>
2017-12-03 23:04:06 +0100

    Added packet tests

--- MQTT/test/packet.jl
+++ MQTT/test/packet.jl
@@ -5,3 +5,18 @@ using mqtt
     header = mqtt.mqttheader()
     @test header.data == 0x030
 end
+
+@testset "mqttPacketType" begin
+    header = mqtt.mqttheader()
+    @test header.data > 4
+end
+
+@testset "getRetained" begin
+    header = mqtt.mqttheader()
+    @test mqtt.getRetained() == header.data & 0x01
+end
+
+@testset "getQos" begin
+    header = mqtt.mqttheader()
+    @test header.data > 4
+end
