da508c34d8392e24a7db9c2cc3ebee05a26bf6dc
KatieObrien <katie-obrien96@hotmail.com>
2017-12-04 18:30:31 +0100

    Add files via upload
    Created tests for packet

--- MQTT/test/packet.jl
+++ MQTT/test/packet.jl
@@ -1,27 +1,44 @@
 using Base.Test
 using mqtt
-
+#Mqtt header test
 @testset "mqttheader" begin
     header = mqtt.mqttheader()
     @test header.data == 0x030
 end
 
+#What does it equal???
+#PUBLISH::mqtt.MsgType = 3
+#FIX THIS TeST
 @testset "mqttPacketType" begin
-    header = mqtt.mqttheader()
-    @test header.data > 4
+    head = mqtt.mqttheader()
+    header = mqtt.mqttPacketType(head)
+    @test header != 3
 end
 
+#Get retained test
 @testset "getRetained" begin
-    header = mqtt.mqttheader()
-    @test mqtt.getRetained(header) == header.data & 0x01
+    head = mqtt.mqttheader()
+    h = mqtt.getRetained(head)
+    @test h == 0x00 & 0x01
 end
 
+#Get DUP test
 @testset "getDup" begin
-    header = mqtt.mqttheader()
-    @test mqtt.getDup(header) ==  header.data & 0x08
+    head = mqtt.mqttheader()
+    h = mqtt.getDup(head)
+    @test h == 0x00 & 0x08
 end
 
+#Get Qos test
+#Onceandonlyonce::mqtt.MqttQos = 3
 @testset "getQos" begin
-    header = mqtt.mqttheader()
-    @test header.data > 4
+    head = mqtt.mqttheader()
+    h = mqtt.getQos(head)
+    @test h != 3
+end
+
+#ConnectFlags test
+@testset "ConnectFlags" begin
+      flag = mqtt.ConnectFlags()
+      @test flag.flags == 0x08
 end
