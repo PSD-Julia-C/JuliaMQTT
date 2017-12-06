ab617997be643e6a237f949da77b9e35dad022e3
KatieObrien <katie-obrien96@hotmail.com>
2017-12-06 13:43:41 +0100

    Got tests working/done
    Some tests not working as source code for cycle, waitfor and read packet need to be debugged

--- MQTT/test/mqttClient.jl
+++ MQTT/test/mqttClient.jl
@@ -9,27 +9,86 @@ using mqtt
 end
 
 #MQTT connect test
-"""@testset "MQTTConnect" begin
+@testset "MQTTConnect" begin
     c = mqtt.MQTTClient()
     opt = mqtt.MQTTPacketConnectData()
     con = mqtt.MQTTConnect(c,opt)
-    @test con == false
-end"""
+    #f = mqtt.MqttReturnCode(-1)
+    @test con = true
+end
 
-#Mqtt publish test
-#not working
-"""@testset "MQTTPublish" begin
-    c = mqtt.MQTTClient()
-    msg = mqtt.MQTTMessage()
-    pub = mqtt.MQTTPublish(c,msg)
-    @test pub = 0
-end"""
+#Mqtt Publish test
+#@testset "MQTTPublish" begin
+#    c = mqtt.MQTTClient()
+#    msg = mqtt.MQTTMessage()
+#    pub = mqtt.MQTTPublish(c,msg)
+#    s = mqtt.MqttReturnCode(0)
+#    @test pub = s
+#end
 
+#MQTT subsccribe test
 @testset "MQTTSubscribe" begin
     c = mqtt.MQTTClient()
+    q = mqtt.MqttQoS(2)
+    handler = x->x*x
     top = "Topic"
-    qos = mqtt.MqttQos(2)
-    handler = function
-    sub = mqtt.MQTTSubscribe(c,top,qos,handler)
-    @test sub == 0
+    sub = mqtt.MQTTSubscribe(c,top,q,handler)
+    f = mqtt.MqttReturnCode(-1)
+    @test sub == f
 end
+
+#MQTT unsubscribe test
+@testset "MQTTUnsubscribe" begin
+    c = mqtt.MQTTClient()
+    topicN = "Goodbye"
+    un = mqtt.MQTTUnsubscribe(c,topicN)
+    s = mqtt.MqttReturnCode(0)
+    @test un = true
+end
+
+#MQTT Disconnect test
+@testset "MQTTDisconnect" begin
+    c = mqtt.MQTTClient()
+    dis = mqtt.MQTTDisconnect(c)
+    @test dis = true
+end
+
+#MQTT Yield test
+#rc in method is saying its not defined might be while loop
+#@testset "MQTTYield" begin
+#    c = mqtt.MQTTClient()
+#    t = 8
+#    y = mqtt.MQTTYield(c,t)
+#    @test y == true
+#end
+
+#waitfor test
+#@testset "waitfor" begin
+#    c = mqtt.MQTTClient()
+#    p = "PUBACK"
+#    t = mqtt.Timer(8)
+#    w = mqtt.waitfor(c,p,t)
+#    f = mqtt.MqttReturnCode(-1)
+#    @test w == f
+#end
+
+#deliverMessage test
+#need to pass in a message
+#    c = MQTTClient()
+
+#end
+
+#keep alive function
+@testset "keepalive" begin
+    c = MQTTClient()
+    k = mqtt.keepalive(c)
+    @test k == nothing
+end
+
+#cycle test
+@testset "cycle" begin
+    c = mqtt.MQTTClient()
+    t = mqtt.Timer(7)
+    c = mqtt.cycle(c,t)
+    @test c = ""
+end 
