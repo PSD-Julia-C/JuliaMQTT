82ba8ae6a7ff206f747db4372e79fc1fde3149cb
KatieObrien <katie-obrien96@hotmail.com>
2017-12-10 16:14:27 +0100

    Added more tests

--- MQTT/test/matchSubject.jl
+++ MQTT/test/matchSubject.jl
@@ -5,5 +5,19 @@ try
   @testset "serialize" begin
     @test mqtt.isTopicMatched("/hugo", "/hugo")
   end
+
+  @testset "connect" begin
+    @test mqtt.isTopicMatched("Hello","Hello")
+  end
+
+  @testset "subscribe" begin
+    @test mqtt.isTopicMatched("foo/bar", "foo/#")
+  end
+
+  @testset "subscribe" begin
+    #s = "non/match"
+    m = mqtt.isTopicMatched("non/match", "non/+/+")
+    @test m == false
+  end
 catch
 end
