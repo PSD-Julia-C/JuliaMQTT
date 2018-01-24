cbe8d25a492122e2ad2657d4378c53f89c6cd49a
TomMcNulty97 <c15480708@mydit.ie>
2018-01-24 01:31:47 +0100

    Fixed Merge

--- MQTT/src/mqttClient.jl
+++ MQTT/src/mqttClient.jl
@@ -20,13 +20,9 @@ function MQTTConnect(client::MQTTClient, options::MQTTPacketConnectData = MQTTPa
 
         client.ping_timer = Timer(client.keepAliveInterval)
         len = serializeConnect(client.buf, client.buf_size, options)
-<<<<<<< HEAD
-        sendPacket(client, len,Timer(client.command_timeout_ms))   #changed command_timeout to command_timeout_ms
-=======
+
         timer = Timer(client.command_timeout_ms) #changed command_timeout to command_timeout_ms
         sendPacket(client, len, timer)
-        println(Timer)
->>>>>>> ddc4c4ee01d9885a2cabfa2a39c87f773d40ee45
         #this will be a blocking call, wait for the connack
         waitfor(client, CONNACK, timer)   #changed timer name to command_timeout_ms (errors occured)
         (rc, session) = deserializeConnack(client.readbuf, client.readbuf_size)
